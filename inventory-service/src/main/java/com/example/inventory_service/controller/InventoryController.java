8edfc4f9f3985793932e8862a6cf257bc91fc745
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-01-19 17:14:08 +0530

    add stock management functionality to inventory service

--- inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
@@ -1,5 +1,15 @@
 package com.example.inventory_service.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.inventory_service.entity.Product;
+import com.example.inventory_service.service.InventoryService;
+
 @RestController
 @RequestMapping("/api/v1/inventory")
 public class InventoryController {
@@ -12,8 +22,13 @@ public class InventoryController {
 
     @GetMapping("/check")
     public boolean checkStock(
-            @RequestParam Long productId,
+            @RequestParam String productId,
             @RequestParam int quantity) {
         return inventoryService.isInStock(productId, quantity);
+    }
+
+    @PostMapping("/addStock")
+    public Product addProduct(@RequestBody Product product) {
+        return inventoryService.addProduct(product);
     }
 }
--- inventory-service/src/main/java/com/example/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/example/inventory_service/service/InventoryService.java
@@ -20,4 +20,8 @@ public class InventoryService {
 
         return product.getStock() >= quantity;
     }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
 }
