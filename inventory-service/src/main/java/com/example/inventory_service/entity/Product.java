eb9e76df1721e6ee245788cc9a97d6b83c394f1e
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-01-19 17:06:54 +0530

    updated product service with product entity

---
+++ inventory-service/src/main/java/com/example/inventory_service/entity/Product.java
@@ -0,0 +1,28 @@
+package com.example.inventory_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer stock;
+
+    // getters & setters
+}
