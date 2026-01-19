72ae1ce736db3a8dc7ef0c3034148b944ff85ac7
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-01-19 21:50:39 +0530

    add JwtUtil class for JWT token generation

---
+++ user-service/src/main/java/com/example/user_service/utils/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.example.order_service.utils;
+
+import com.example.user_service.entity.AppUser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET = "my-super-secret-key";
+
+    public String generateToken(AppUser user) {
+        return Jwts.builder()
+                .setSubject(user.getId().toString())
+                .claim("role", user.getRole())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1h
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
